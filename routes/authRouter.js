11ed7b4469f86b556da59580aa96e500bb49b493
David Saperstein <d.h.saperstein@gmail.com>
2021-12-27 12:55:47 -0700

    updated user model. changed username to email and added name property

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,14 +4,14 @@ const User = require('../models/user.js')
 const jwt = require('jsonwebtoken')
 
 authRouter.post("/signup", (req, res, next) => {
-  User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+  User.findOne({ email: req.body.email.toLowerCase() }, (err, user) => {
     if(err){
       res.status(500)
       return next(err)
     }
     if(user){
       res.status(403)
-      return next(new Error("That username is already taken"))
+      return next(new Error("That email is already taken"))
     }
     const newUser = new User(req.body)
     newUser.save((err, savedUser) => {
@@ -26,24 +26,24 @@ authRouter.post("/signup", (req, res, next) => {
 
 //Login
 authRouter.post("/login", (req, res, next) => {
-  User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
+  User.findOne({ email: req.body.email.toLowerCase() }, (err, user) => {
     if(err){
       res.status(500)
       return next(err)
     }
     if(!user){
       res.status(403)
-      return next(new Error("Username or Password are incorrect"))
+      return next(new Error("email or Password are incorrect"))
     }
     
     user.checkPassword(req.body.password, (err, isMatch) => {
       if(err){
         res.status(403)
-        return next(new Error("Username or Password are incorrect"))
+        return next(new Error("email or Password are incorrect"))
       }
       if(!isMatch){
         res.status(403)
-        return next(new Error("Username or Password are incorrect"))
+        return next(new Error("email or Password are incorrect"))
       }
       const token = jwt.sign(user.withoutPassword(), process.env.SECRET) 
       return res.status(200).send({ token, user: user.withoutPassword() })
